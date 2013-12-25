bbb13a960686311c8b9e86ea9b0cac9b40d19da3
shenwei Wang <microsalience@gmail.com>
2013-12-25 17:56:00 +0800

    udate gethello

--- ch03/gethello.js
+++ ch03/gethello.js
@@ -1,5 +1,5 @@
-var hello = require("./singleobject").Hello;
-//var hello = require("./Hello");
+//var hello = require("./singleobject").Hello;
+var hello = require("./singleobject");//需要写成require(jsFileName);书上勘误
 var hello_obj = new hello();// 书上勘误
 hello_obj.setName("charles");
 hello_obj.sayHello();
