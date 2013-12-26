f3df49c3be8f4b7d1c8209649453715aac46bbe8
Shenwei Wang <microsalience@gmail.com>
2013-12-26 20:22:12 +0800

    http module commit

---
+++ ch04/httpserverrequestget.js
@@ -0,0 +1,11 @@
+// 在浏览器中访问 http://127.0.0.1:3000/user?name=byvoid&email=byvoid@byvoid.com
+
+var http = require('http');
+var url = require('url');
+var util = require('util');
+http.createServer(function(req, res) {
+			res.writeHead(200, {
+						'Content-Type' : 'text/plain'
+					});
+			res.end(util.inspect(url.parse(req.url, true)));
+		}).listen(3000);
