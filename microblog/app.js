b5b97db9f81311c78523818731e4054516f5e6ed
shenwei Wang <microsalience@gmail.com>
2013-12-27 15:15:08 +0800

    回滚提交

--- microblog/app.js
+++ microblog/app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+//
+var partials = require('express-partials');
 
 var app = express();
 
@@ -15,6 +17,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,14 +26,36 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * 3.x不支持 // 视图静态助手 app.helpers({ inspect : function(obj) { return
+ * util.inspect(obj, true); } });
+ *  // 视图动态助手 app.dynamicHelpers({ headers : function(req, res) { return
+ * req.headers; } });
+ */
+
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
+app.get('/hello', routes.hello);
+app.get('/user/:username', function(req, res) {
+			res.send("username: " + req.params.username);
+		});
 app.get('/users', user.list);
+app.get('/list', function(req, res) {
+			res.render("list", {
+						title : 'List',
+						items : [1991, 'byvoid', 'express', 'Node.js']
+					});
+		});
+app.get('/help', function(req, res) {
+			res.render('help', {
+						title : 'Helps'
+					});
+		});
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function() {
+			console.log('Express server listening on port ' + app.get('port'));
+		});
--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -1,8 +1,13 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+	res.render('index', {
+		title : 'Express'
+	});
+};
+
+exports.hello = function(req, res) {
+	res.send('The time is ' + new Date().toString());
 };
