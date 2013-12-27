9d6b3cf059923880f6d3569dc3708bd33edc9ffe
shenwei Wang <microsalience@gmail.com>
2013-12-27 16:18:11 +0800

    增加bootstrap,jQuery

--- microblog/app.js
+++ microblog/app.js
@@ -2,7 +2,6 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -28,9 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * 3.x不支持 // 视图静态助手 app.helpers({ inspect : function(obj) { return
- * util.inspect(obj, true); } });
- *  // 视图动态助手 app.dynamicHelpers({ headers : function(req, res) { return
- * req.headers; } });
+ * util.inspect(obj, true); } }); // 视图动态助手 app.dynamicHelpers({ headers :
+ * function(req, res) { return req.headers; } });
  */
 
 // development only
@@ -39,10 +37,19 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/user/:username', routes.user);
+app.get('/post', routes.post);
+app.get('/reg', routes.reg);
+app.post('/reg', routes.doReg);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
 app.get('/hello', routes.hello);
-app.get('/user/:username', function(req, res) {
-			res.send("username: " + req.params.username);
-		});
+/**
+ * app.get('/user/:username', function(req, res) { res.send("username: " +
+ * req.params.username); });
+ */
+
 app.get('/users', user.list);
 app.get('/list', function(req, res) {
 			res.render("list", {
--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -8,6 +8,34 @@ exports.index = function(req, res) {
 	});
 };
 
+exports.user = function(req, res) {
+	res.send('user!');
+};
+
+exports.post = function(req, res) {
+	res.send('post!');
+};
+
+exports.reg = function(req, res) {
+	res.send('reg!');
+};
+
+exports.doReg = function(req, res) {
+	res.send('doReg!');
+};
+
+exports.login = function(req, res) {
+	res.send('login!');
+};
+
+exports.doLogin = function(req, res) {
+	res.send('doLogin!');
+};
+
+exports.logout = function(req, res) {
+	res.send('logout!');
+};
+
 exports.hello = function(req, res) {
 	res.send('The time is ' + new Date().toString());
 };
